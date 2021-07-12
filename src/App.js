c55d7b5b9106d07f9e5303399452edb84bca5588
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-07-12 22:41:49 +0200

    Navbar & Footer Added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,40 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //App Components
 import Navbar from "./components/general/Navbar.js";
 import Footer from "./components/general/Footer.js";
+import { Container } from "react-bootstrap";
 
 //Pages
-import Dashboard from "./components/pages/Dashboard.js"
+import Dashboard from "./components/pages/Dashboard.js";
 import LogInPage from "./components/pages/LogInPage.js";
 
 function App() {
   return (
-    <><p>Admin Front-End</p>
-      <Navbar />
+    <>
       <Router>
-        <div>
-          <Switch>
-            <Route exact path="/log-in"> {/* Natasha */}
-              <LogInPage /> 
-            </Route>
-            <Route exact path="/dashboard"> {/* Natasha */}
-              <Dashboard/>
-            </Route>
-          </Switch>
-        </div>
+        <Navbar />
+        <Container fluid="xl" style={{ padding: "0" }}>
+          <div>
+            <Switch>
+              <Route exact path="/log-in">
+                {" "}
+                {/* Natasha */}
+                <LogInPage />
+              </Route>
+              <Route exact path="/dashboard">
+                {" "}
+                {/* Natasha */}
+                <Dashboard />
+              </Route>
+              <Route exact path="/">
+                {" "}
+                {/* Natasha */}
+                <Dashboard />
+              </Route>
+            </Switch>
+          </div>
+        </Container>
+        <Footer />
       </Router>
-      <Footer />
     </>
   );
 }
