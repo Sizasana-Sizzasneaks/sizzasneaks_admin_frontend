0931e3a086f87613324dfda607c839095735ace9
Myth-skyie <khumalonatasha179@gmail.com>
2021-07-20 13:11:26 +0200

    fixedAuthForLogin

--- src/components/pages/LogInPage.js
+++ src/components/pages/LogInPage.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
 import {
   Grid,
   Paper,
@@ -16,23 +15,100 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import useStyles from "./styles/style.js";
 
-function LogInPage() {
+import * as Yup from "yup";
+import { useFirebase } from "react-redux-firebase";
+
+
+const LogInPage = ({showLogin}) => {
+
   const classes = useStyles();
   const paperStyle = {
     padding: 20,
-    height: "55vh",
+    height: "70vh",
     width: 700,
     margin: "20px auto",
   };
   const avatarStyle = { backgroundColor: "#02ced1" };
   const tstyle = { margin: "20px", width: 400 };
-  const bstyle = { margin: "8px 0", backgroundColor: "#02ced1" };
+  const bstyle = { margin: "8px 0", backgroundColor: "orange" };
+
+  //Validation start
+  const emailSchema = Yup.object().shape({
+    email: Yup.string("Enter String")
+    .email("Invalid email address")
+    .required("Required").nullable()
+  });
+
+  const passwordSchema = Yup.object().shape({
+    password: Yup.string("Enter String")
+    .required("Required"),
+  });
+
+
+    const firebase = useFirebase();
+
+    var [email, setEmail] = useState(null);
+    var [password, setPassword] = useState("");
+
+    var [errorEmail, setErrorEmail] = useState(null);
+    var [errorPassword, setErrorPassword] = useState(null);
+    
+    useEffect(() => {
+      validateEmail();
+      validatePassword();
+      // return () => {
+      //   cleanup
+      // };
+    }, [email, password]);
+ 
+  
+
+  function logIn(email, password) {
+    
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log("user signedIn");
+      })
+      .catch((error) => {
+        console.log("Log In Error");
+      });
+  }
+
+  function validateEmail() {
+    emailSchema
+      .validate({ email: email })
+      .then(() => {
+        console.log("email validated");
+        setErrorEmail({ valid: true, message: "" });
+      })
+      .catch((error) => {
+        setErrorEmail({ valid: false, message: error.errors[0] });
+      });
+  }
+
+  function validatePassword() {
+    passwordSchema
+      .validate({ password: password })
+      .then(() => {
+        setErrorPassword({ valid: true, message: ""});
+      })
+      .catch((error) => {
+        setErrorPassword({ valid: false, message: error.errors[0] });
+      });
+  }
+
+
+
 
   return (
+
+    <div className = {`${showLogin ? "active" : ""} show`}>
     <form className={classes.root} noValidate autoComplete="off">
       <div className="LogInPage" noValidate autoComplete="Off">
         <Grid>
-          <Paper elevation={10} style={paperStyle}>
+          <Paper elevation={20} style={paperStyle}>
             <Grid align="center">
               <Avatar style={avatarStyle}>
                 <PersonIcon />
@@ -43,21 +119,35 @@ function LogInPage() {
             <Box align="center">
               <Container>
                 <TextField
-                  label="Email"
                   placeholder="Enter email"
                   variant="outlined"
                   style={tstyle}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
                 />
+              <p>
+                {errorEmail && (!errorEmail.valid ? errorEmail.message : "")}
+              </p>
+
               </Container>
+              
               <Container>
                 <TextField
-                  label="Password"
                   placeholder="Enter password"
                   type="password"
                   style={tstyle}
                   variant="outlined"
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
                 />
+
+                <p>
+                 {errorPassword && (!errorPassword.valid ? errorPassword.message : "")}
+              </p>
               </Container>
+               
 
               <Container>
                 <FormControlLabel
@@ -74,10 +164,12 @@ function LogInPage() {
               </Container>
 
               <Button
-                type="submit"
                 color="primary"
                 style={bstyle}
                 variant="contained"
+                onClick={() => {
+                  logIn(email, password);
+                }}
               >
                 Submit
               </Button>
@@ -90,7 +182,9 @@ function LogInPage() {
         </Grid>
       </div>
     </form>
+    </div>
   );
-}
+};
+
 
 export default LogInPage;
