97f0482a1320b4a59d2be6bf22e3bd6655678364
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-07-28 20:02:44 +0200

    Admin Progress

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -1,24 +1,45 @@
 import { getFirebase } from "react-redux-firebase";
-import {getUserDetails} from "../api/user"
+import { getUserDetails } from "../api/user";
 
-export const logIn=(email, password)=>{
-    var firebase=getFirebase();
-  
-    getUserDetails();
+export const logIn = (email, password) => {
+  var firebase = getFirebase();
 
-    return firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((user) => {
-        console.log("admin signedIn");
-        return { ok: true, message: null };
-      })
-      .catch((error) => { 
-        console.log("Log In Error");
-        return { ok: false, message: "Log In Error" };
-      });
-  }
-  
+  //getUserDetails();
+
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(() => {
+      return firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((user) => {
+          console.log("admin signedIn");
+          return { ok: true, message: null };
+        })
+        .catch((error) => {
+          console.log("Log In Error");
+          return { ok: false, message: "Log In Error" };
+        });
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+
+  // return firebase
+  //   .auth()
+  //   .signInWithEmailAndPassword(email, password)
+  //   .then((user) => {
+  //     console.log("admin signedIn");
+  //     return { ok: true, message: null };
+  //   })
+  //   .catch((error) => {
+  //     console.log("Log In Error");
+  //     return { ok: false, message: "Log In Error" };
+  //   });
+};
+
 //get CurrentUserIDToken
 export const getCurrentUserIdToken = () => {
   var firebase = getFirebase();
@@ -29,7 +50,6 @@ export const getCurrentUserIdToken = () => {
     .then((idToken) => {
       console.log("something happened");
       return { ok: true, data: idToken };
-      
     })
     .catch((error) => {
       console.log("nothing happened");
