36397826f20884a705230b6de20591790d3175fd
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-08-09 19:14:43 +0200

    Ready for deploy

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-import { firebaseCredentials } from "../secrets";
+const firebaseConfig = {
+    apiKey: "AIzaSyC93DVOcFvAhxDrcivdomIHf8cK86z_txI",
+      authDomain: "sizasana-ecommerce-admin.firebaseapp.com",
+      projectId: "sizasana-ecommerce-admin",
+      storageBucket: "sizasana-ecommerce-admin.appspot.com",
+      messagingSenderId: "687970957165",
+      appId: "1:687970957165:web:5ce246aae4b4b77a9bcc64"
+    };
 
-const firebaseConfig = firebaseCredentials;
 
 firebase.initializeApp(firebaseConfig);
 
