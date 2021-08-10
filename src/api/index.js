77fc9d93405188155499fac6c7bd8e38b75bf073
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-08-10 13:13:29 +0200

    Added Delete Review Function

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,5 @@ export const BACKEND_ROUTE = "http://localhost:5000"
 export const USER_ROUTE = BACKEND_ROUTE + "/user";
 
 export const PRODUCTS_ROUTE = BACKEND_ROUTE + "/products";
+
+export const REVIEWS_ROUTE = BACKEND_ROUTE + "/reviews";
